d16b241f23f57327c9ce1eff11e00ba4104ea19d
Jaikrat Singh Tariyal <tektutorialfeedback@gmail.com>
2021-09-09 18:21:58 +0530

    change of file name and removal of fields in schema
    change of file name and removal of fields in schema

--- kong/plugins/check-x-header/schema.lua
+++ kong/plugins/check-x-header/schema.lua
@@ -1,15 +1,15 @@
 local typedefs = require "kong.db.schema.typedefs"
 
--- Grab pluginname from module name
+-- Grab plugin name from module name
 local plugin_name = ({...})[1]:match("^kong%.plugins%.([^%.]+)")
 
 
 local schema = {
   name = plugin_name,
   fields = {
-    { route = typedefs.no_route },
-    { service = typedefs.no_service },
-    { consumer = typedefs.no_consumer },
+    -- { route = typedefs.no_route },
+    -- { service = typedefs.no_service },
+    -- { consumer = typedefs.no_consumer },
     { protocols = typedefs.protocols_http },
     { config = {
       type = "record",
